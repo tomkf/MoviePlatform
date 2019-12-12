54f2de93c7348dc99d51d56733258bbbcf21123f
Thomas Ferris <A01211467@ad.bcit.ca>
2019-12-11 16:42:02 -0800

    modular styles + search bar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import Nav from './Nav'
+import InputGroup  from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl'
 
 class Home extends React.Component {
     render() {
-      return <div>
+      return (
+      <div>
         <Nav></Nav>
-        <div className="contentWrap"> 
-        <h1>Home</h1>
-        </div>
-        </div>
+      <div className="contentWrap"> 
+       <h1>Home</h1>
+      <section className="searchBar"> 
+        <InputGroup className="mb-3">
+         <InputGroup.Prepend>
+          <InputGroup.Text>Search movie by title</InputGroup.Text>
+         </InputGroup.Prepend>
+           <FormControl />
+        </InputGroup>
+      </section>
+       </div>
+      </div>
+      );
     }
   }
 
