1af3e1d81b91ff6f404cc4d9fe491d41715bb028
Thomas Ferris <A01211467@ad.bcit.ca>
2019-12-16 18:07:18 -0800

    more info for cards, next route to film page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Nav from './Nav'
+import genericMovie from '../images/genericMovie.png'
 
 //bootstrap components
 import InputGroup  from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
@@ -49,20 +51,75 @@ class Home extends React.Component {
     this.setState((prevState) => { return { films: workingState, render: true}})
   }
 
+  routeFilm(filmObj){
+    console.log(filmObj)
+  }
+
   //return rendered list of movies
   renderRes(items){
     let filmArr =  items.map(film =>  ( 
-  <Card style={{ width: '18rem' }} className="filmCard">
-  <Card.Img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${film.poster_path}`}  variant="top"/>
+  <Card bg="light" style={{ width: '20rem' }} className="filmCard" onClick={this.routeFilm(film)}>
+  <Card.Img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${film.poster_path}`} alt={genericMovie}  variant="top"/>
   <Card.Body>
-    <Card.Title> {film.title} </Card.Title>
+    <Card.Title> {film.title} </Card.Title> 
+    <Card.Subtitle className="rating"> Users Rating: {film.vote_average * 10} % </Card.Subtitle>
+    <Card.Subtitle className="rating"> {this.parseDate(film.release_date)} </Card.Subtitle>
     <Card.Text>
      {film.overview}
     </Card.Text>
   </Card.Body>
+  <Button variant="primary"> Click to find out more. </Button>
 </Card>
      ));
     return (  <div className="titlesContainer"> { filmArr} </div>)
+  }
+
+  parseDate(dateStr){
+  let year = dateStr.slice(0, 4)
+  let month = dateStr.slice(5, 7)
+  let day = dateStr.slice(8, 10)
+  let parseMonth = ""
+
+  switch(month){
+    case "01":
+      parseMonth = "January";
+    break;
+    case "02":
+        parseMonth = "Febuary";
+    break;
+    case "03":
+        parseMonth = "March";
+    break;
+    case "04":
+        parseMonth = "April";
+    break;
+    case "05":
+        parseMonth = "May";
+    break;
+    case "06":
+        parseMonth = "June";
+    break;
+    case "07":
+        parseMonth = "July";
+    break;
+    case "08":
+        parseMonth = "August";
+    break;
+    case "09":
+        parseMonth = "September";
+    break;
+    case "10":
+        parseMonth = "October";
+    break;
+    case "11":
+        parseMonth = "November";
+    break;
+    case "12":
+        parseMonth = "December";
+    break;
+  }
+
+  return `${parseMonth} ${day}, ${year}`
   }
 
   changeTitle = title =>  this.setState((prevState) => { return { searchTitle: title} })
