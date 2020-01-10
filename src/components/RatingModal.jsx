a31b2a24a5f37736b7aa6037ceb6a1cbcfb5c593
tomkf <thomaskeithferris@gmail.com>
2020-01-09 16:00:18 -0800

    working on rating system

--- src/components/RatingModal.jsx
+++ src/components/RatingModal.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactModal from 'react-modal';
 
+import StarRatingComponent from 'react-star-rating-component';
+// docs: https://www.npmjs.com/package/react-star-rating-component
+
+
 class RatingModal extends React.Component {
 
     
@@ -14,7 +18,7 @@ class RatingModal extends React.Component {
         return( 
              <div> <ReactModal isOpen={this.props.openWindow}  style={{ overlay: {backgroundColor: "rgb(102, 120, 156)", margin: "6%", borderRadius: "10px"}, content: { width: "82vw"} } } > 
             <h2>Rate this movie: </h2>
-            
+
             <button onClick={this.props.closeModal}> Cancel </button>
 
              </ReactModal> 
