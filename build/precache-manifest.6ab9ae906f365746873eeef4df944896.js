fa4b51d415c3f5e499491b9ed8486214245de8ea
tomkf <thomaskeithferris@gmail.com>
2020-01-13 23:36:48 -0800

    production folder pushed

---
+++ build/precache-manifest.6ab9ae906f365746873eeef4df944896.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211bf22036d81296f4f1e504595d2ebb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1f1c8d4b63a17643a89",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ae9444d30b6654a0f35d",
+    "url": "/static/css/main.a65d3bf5.chunk.css"
+  },
+  {
+    "revision": "c1f1c8d4b63a17643a89",
+    "url": "/static/js/2.4ae4b452.chunk.js"
+  },
+  {
+    "revision": "757dee81ba33cf81c8a0f3462dbeffa0",
+    "url": "/static/js/2.4ae4b452.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ae9444d30b6654a0f35d",
+    "url": "/static/js/main.7949d48d.chunk.js"
+  },
+  {
+    "revision": "f8dd1d43eeaabda12f79",
+    "url": "/static/js/runtime-main.94217d0c.js"
+  },
+  {
+    "revision": "27b65cb40d26f78354a4ac5abf87b2be",
+    "url": "/static/media/dblogo.27b65cb4.svg"
+  },
+  {
+    "revision": "96a00d42b0ff8f80b7cdf2926a211e47",
+    "url": "/static/media/genericMovie.96a00d42.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "58dc5e36dbd3c391997ba57c8ff1bf0d",
+    "url": "/static/media/movieLogo.58dc5e36.png"
+  }
+]);
